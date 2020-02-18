4608fd19ab2bf7c7344bfc7feeb7dbbeddfb0ccf
JeffRThomas <thomas.jeff@live.com>
2020-02-18 13:44:15 -0600

    Exercise1&2Done

---
+++ jvstemplatecw.js
@@ -0,0 +1,27 @@
+// Kevin's Sanity Check
+console.log("Your javascript file is linked correctly.");
+
+// ### Exercise 1:
+// *Start of Code*
+// Variable Declaration
+let my_name = "Kevin";
+let my_id = "2112"
+let my_address = "Memphis, TN"
+// Template Literal String Creation
+let my_template_string = (`My name is ${my_name}
+My id is ${my_id}
+My address is ${my_address}.`);
+// Print Output to Console Log
+console.log(my_template_string);
+// *End of Code*
+
+// ### Exercise 2:
+// *Start of Code*
+// Copied Predeclared Variables From Instructions
+var contestant = "1_M_l337!";
+var score = "9001";
+// Use of Template Literal String To Alert User The OUTPUT
+let congrats2U = (`Congratulations ${contestant} !!, your score is ${score}`);
+alert(congrats2U);
+// *End of Code*
+
